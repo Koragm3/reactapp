51a6bdf89a17b4203a7b5825a69ca01f8d252543
Koragm3 <hasanserro@gmail.com>
2023-11-19 18:07:13 +0100

    api added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import { json } from 'react-router-dom';
 
 const MyForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const newErrors = {};
@@ -15,26 +17,45 @@ const MyForm = () => {
     }
     if (email.trim() === '') {
       newErrors.email = 'Email is required';
+
     }
     if (message.trim() === '') {
-      newErrors.message = 'Message is required';
+      newErrors.message = 'Message can not be empty';
     }
-
+    
     
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
       return;
     }
-
-   
     console.log('Form submitted:', { name, email, message });
 
     setName('');
     setEmail('');
     setMessage('');
     setErrors({});
-  };
+    const user = {name, email, message}
+    const json = JSON.stringify(user)
 
+    const result = await fetch ("https://win23-assignment.azurewebsites.net/api/contactform", {
+        method: 'post',
+        headers:{ 'content-type': 'application/json'},
+        body: json
+    })
+
+    switch (result.status){
+        case 201:
+            console.log('Message deliverd')
+            setSuccess('data sent');
+            break;
+        case 400:
+            console.log('Something went wrong.' + await result.text())
+        
+    }
+
+
+  };
+  
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -55,9 +76,11 @@ const MyForm = () => {
         
         <textarea  name="YourMessage" onChange={(e) => setMessage(e.target.value)} value={message}  placeholder="Your Message*"/> <br/>
 
-        {errors.password && <p className="error">{errors.message}</p>}
+        {errors.message && <p className="error">{errors.message}</p>}
+         
+        {success && <p className="error">{success}</p>}
       </div>
-
+        <div></div>
       <button type="submit">Submit</button>
     </form>
   );
